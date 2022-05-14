e8f8a7aea460670d396df6707228f456f4818465
Denis <103826405+Ednis1985@users.noreply.github.com>
2022-05-14 15:50:12 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-p = float(input())#процентная ставка
-x = int(input())#money"вклад"
-#y = int(input())
-money_before = 100 * x
-money_after = int(money_before * (100 + p) / 100)
-print(money_after // 100, money_after % 100)
